a112bd1b97fda0e0582399a1db2b90507c7c3370
gustavo-tomas <guga.tomas10@gmail.com>
2023-03-06 15:52:38 -0300

    Testando asserções/logger

--- sandbox/src/main.c
+++ sandbox/src/main.c
@@ -1,9 +1,16 @@
-#include <test.h>
-#include <stdlib.h>
+#include <core/logger.h>
+#include <core/asserts.h>
 
 int main()
 {
-    print_int(443);
-    malloc(13);
+    MGM_FATAL("Deu merda grande: %f", 2.14f);
+    MGM_ERROR("Deu merda: %f", 3.14f);
+    MGM_WARN("Fez merda: %f", 4.14f);
+    MGM_INFO("Informando: %f", 5.14f);
+    MGM_DEBUG("Debugando: %f", 6.14f);
+    MGM_TRACE("Traçando: %f", 7.14f);
+
+    MGM_ASSERT(1 == 0, "1 não é 0 :(");
+
     return 0;
 }
