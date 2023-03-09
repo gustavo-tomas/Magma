ff778842ea4c8b82b8c0c816456e12b46377e551
gustavo-tomas <guga.tomas10@gmail.com>
2023-03-09 14:21:05 -0300

    Renomeando

--- sandbox/src/entry_point.c
+++ sandbox/src/entry_point.c
@@ -3,20 +3,20 @@
 #include <entry_point.h>
 
 // Ponto de entrada para criar o jogo
-b8 create_game(game* out_game)
+b8 create_game(game* game)
 {
-    out_game->app_config.start_pos_x = 100;
-    out_game->app_config.start_pos_y = 100;
-    out_game->app_config.start_width = 1280;
-    out_game->app_config.start_height = 720;
-    out_game->app_config.name = "Magma Sandbox";
+    game->app_config.start_pos_x = 100;
+    game->app_config.start_pos_y = 100;
+    game->app_config.start_width = 1280;
+    game->app_config.start_height = 720;
+    game->app_config.name = "Magma Sandbox";
 
-    out_game->initialize = initialize_game;
-    out_game->update = update_game;
-    out_game->render = render_game;
-    out_game->on_resize = on_resize_game;
+    game->initialize = initialize_game;
+    game->update = update_game;
+    game->render = render_game;
+    game->on_resize = on_resize_game;
 
-    out_game->state = mgm_allocate(sizeof(game_state), MEMORY_TAG_GAME);
+    game->state = mgm_allocate(sizeof(game_state), MEMORY_TAG_GAME);
 
     return TRUE;
 }
